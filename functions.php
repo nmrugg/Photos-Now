65899eaa0c95d1ab7a1c0aa286786d53352c5630
N8 <nmrugg@gmail.com>
2009-12-24 09:51:54 -0600

    Picture piles for folders working now.
    Still need to add text and polish.

--- functions.php
+++ functions.php
@@ -8,12 +8,12 @@ define('PHOTO_PILE_COUNT', 5);
 define('PHOTO_SIZE', 175);
 
 /// Global variables
-$thumb_start = '<ul class=gallery>';
-$thumb_top1 = '<li><a href="#show(\'';
+$thumb_start = '';
+$thumb_top1 = '<div class=photo_div><a href="#show(\'';
 $thumb_top2 = '\');"><span>&nbsp;</span><img class=background src=".images/polaroid.png"><img class=thumb src="';
 $thumb_middle = '"><em>';
-$thumb_bottom = '</em></a></li>';
-$thumb_end = '</ul>';
+$thumb_bottom = "</em></a></div>\n";
+$thumb_end = "";
 
 function write_header()
 {
@@ -43,130 +43,43 @@ img {
 a {
 	text-decoration: none;
 }
-</style>
-<script src=main.js></script>
-</head><body>
-	<?php
+
+
+/* Start of Picture Pile Style */
+
+.pic_pile {
+	position: absolute;
+}
+.folder {
+	position: absolute;
+	padding-top: 45px;
+}
+.front {
+	z-index: 99;
+}
+.dir {
+	float: left;
+	width: 200px!important;
 }
 
-function show_back($starting_dir)
-{
-	
-}
+/* End of Picture Pile Styles */
 
 
-function list_dirs($dirs)
-{
-	if (count((array)$dirs) == 0) return null;
-	
-	write_dir_style();
-	
-	foreach ($dirs as $dir) {
-		$dir_images = get_images($dir . '/');
-		echo '<div>';
-		echo '<img src=".images/folder-yellow-back.png" class="folder back">';
-		echo '<img src=".images/folder-yellow-front.png" class="folder front">';
-		for ($i = 0; $i < PHOTO_PILE_COUNT; ++$i) {
-			$rand_key = array_rand($dir_images);
-			//echo find_thumb($dir_images[$rand_key]);
-			echo '<img src="' . find_thumb($dir_images[$rand_key]) . '" class="pic_pile" style="-moz-transform: rotate(' . round(mt_rand(-6, 6)) . 'deg); left: ' . (($i) * PHOTO_SIZE * -1) . 'px">';
-		}
-		echo '</div>';
-	}
-}
+/* Start of Pictures Styles */
 
-
-function write_dir_style()
-{
-	?>
-<style>
-	.pic_pile {
-		position: relative;
-	}
-	.folder {
-		position: absolute;
-		padding-top: 45px;
-	}
-	.front {
-		z-index: 99;
-	}
-</style>
-<?php
-}
-
-function get_images($dir)
-{
-	return glob($dir . '*.{j,p,g,J,P,G}{p,n,i,P,N,I}{g,f,G,F}', GLOB_BRACE);
-}
-
-
-function list_photos($files)
-{
-	write_styles();
-	global $thumb_start, $thumb_top1, $thumb_top2, $thumb_middle, $thumb_bottom, $thumb_end;
-	echo $thumb_start;
-	foreach ($files as $file) {
-		$thumb = find_thumb($file);
-		$thumb_top = $thumb_top1 . htmlentities($file) . $thumb_top2;
-		echo $thumb_top . htmlentities($thumb) . $thumb_middle . wordwrap(htmlentities(title_case(str_replace("_", " ", pathinfo($file, PATHINFO_FILENAME)))), 22, "<br>\n", true) . $thumb_bottom;
-	}
-	echo $thumb_end;
-}
-
-
-function find_thumb($file)
-{
-	$path = pathinfo($file, PATHINFO_DIRNAME) . '/' . THUMB_PATH . '/';
-	$filename = pathinfo($file, PATHINFO_BASENAME);
-	if (!is_dir($path)) {
-		mkdir($path);
-	}
-	if (!file_exists($path . $filename)) {
-		create_thumb($file, $path . $filename);
-	}
-	return $path . $filename;
-}
-
-
-function create_thumb($original, $new_filename)
-{
-	/// There are better ways to do this.
-	shell_exec("convert \"" . addslashes($original) . "\" -thumbnail " . PHOTO_SIZE . "x" . PHOTO_SIZE . "\"" . addslashes($new_filename) . "\"");
-	if (!file_exists($new_filename)) {
-		/// Try gd.
-	}
-}
-
-
-function title_case($title)
-{
-	$preps_articles_conjunctions = array('of','a','the','and','an','or','nor','but','is','if','then', 'else','when','at','from','by','on','off','for','in','out','over','to','into','with');
-	$words = explode(' ', $title);
-	foreach ($words as $key => $word) {
-		if ($key == 0 || !in_array($word, $preps_articles_conjunctions))
-		$words[$key] = ucwords(strtolower($word));
-	}
-	
-	$newtitle = implode(' ', $words);
-	return $newtitle;
-}
-
-function write_styles($which = 1)
-{
-	?>
-<style>
 body {
 	margin: 20px auto;
 	padding: 0;
 	background: url(.images/cork-bg.png);
 }
 
-.gallery {
-	list-style: none;
-	margin: 0;
-	padding: 0;
+
+.gallery .photo_div {
+	height: 250px;
+	width: 200px;
+	float: left;
 }
-.gallery li {
+.gallery .photo_div a {
 	padding-bottom: 8px;
 	/*background: url(.images/polaroid.png) no-repeat;
 	background-size: 100%;*/
@@ -203,6 +116,95 @@ body {
 	src: url('.fonts/PISAN.eot');
 	src: local('Pisan Normal'), local('Pisan-Normal'), url('.fonts/PISAN.woff') format('woff'), url('.fonts/PISAN.TTF') format('truetype'), url('.fonts/PISAN.svg#Pisan-Normal') format('svg');
 }
+
+/* End of picture stlyes */
 </style>
+<script src=main.js></script>
+</head><body>
 	<?php
 }
+
+function show_back($starting_dir)
+{
+	
+}
+
+
+function list_dirs($dirs)
+{
+	if (count((array)$dirs) == 0) return null;
+	
+	foreach ($dirs as $dir) {
+		$dir_images = get_images($dir . '/');
+		echo '<div class=dir>&nbsp;';
+		echo '<img src=".images/folder-yellow-back.png" class="folder back">';
+		echo '<img src=".images/folder-yellow-front.png" class="folder front">';
+		for ($i = 0; $i < PHOTO_PILE_COUNT; ++$i) {
+			$rand_key = array_rand($dir_images);
+			//echo find_thumb($dir_images[$rand_key]);
+			//echo '<img src="' . find_thumb($dir_images[$rand_key]) . '" class="pic_pile" style="-moz-transform: rotate(' . round(mt_rand(-6, 6)) . 'deg); left: ' . (($i) * PHOTO_SIZE * -1) . 'px">';
+			echo '<img src="' . find_thumb($dir_images[$rand_key]) . '" class="pic_pile" style="-moz-transform: rotate(' . round(mt_rand(-6, 6)) . 'deg); lef: ' . (($i) * PHOTO_SIZE * -1) . 'px">';
+		}
+		echo "</div>\n";
+	}
+}
+
+
+
+function get_images($dir)
+{
+	return glob($dir . '*.{j,p,g,J,P,G}{p,n,i,P,N,I}{g,f,G,F}', GLOB_BRACE);
+}
+
+
+function list_photos($files)
+{
+	global $thumb_start, $thumb_top1, $thumb_top2, $thumb_middle, $thumb_bottom, $thumb_end;
+	echo $thumb_start;
+	foreach ($files as $file) {
+		$thumb = find_thumb($file);
+		$thumb_top = $thumb_top1 . htmlentities($file) . $thumb_top2;
+		echo $thumb_top . htmlentities($thumb) . $thumb_middle . wordwrap(htmlentities(title_case(str_replace("_", " ", pathinfo($file, PATHINFO_FILENAME)))), 22, "<br>\n", true) . $thumb_bottom;
+	}
+	echo $thumb_end;
+}
+
+
+function find_thumb($file)
+{
+	$path = pathinfo($file, PATHINFO_DIRNAME) . '/' . THUMB_PATH . '/';
+	$filename = pathinfo($file, PATHINFO_BASENAME);
+	if (!is_dir($path)) {
+		mkdir($path);
+	}
+	if (!file_exists($path . $filename)) {
+		create_thumb($file, $path . $filename);
+	}
+	return $path . $filename;
+}
+
+
+function create_thumb($original, $new_filename)
+{
+	/// There are better ways to do this.
+	$res = shell_exec("convert \"" . addslashes($original) . "\" -thumbnail " . PHOTO_SIZE . "x" . PHOTO_SIZE . " \"" . addslashes($new_filename) . "\" 2>&1");
+	
+	if (!file_exists($new_filename)) {
+		/// Try gd.
+	}
+}
+
+
+function title_case($title)
+{
+	$preps_articles_conjunctions = array('of','a','the','and','an','or','nor','but','is','if','then', 'else','when','at','from','by','on','off','for','in','out','over','to','into','with');
+	$words = explode(' ', $title);
+	foreach ($words as $key => $word) {
+		if ($key == 0 || !in_array($word, $preps_articles_conjunctions))
+		$words[$key] = ucwords(strtolower($word));
+	}
+	
+	$newtitle = implode(' ', $words);
+	return $newtitle;
+}
+
--- index.php
+++ index.php
@@ -8,6 +8,8 @@ if (!isset($_REQUEST['dir']) || $_REQUEST['dir'] == "" || substr($_REQUEST['dir'
 
 if ($_REQUEST['dir'] != "") show_back(PHOTOS_PATH . $_REQUEST['dir']);
 
+echo '<div class=gallery>';
 list_dirs(glob(PHOTOS_PATH . $_REQUEST['dir'] . '*', GLOB_ONLYDIR));
 
 list_photos(get_images(PHOTOS_PATH . $_REQUEST['dir']));
+echo '</div>';
